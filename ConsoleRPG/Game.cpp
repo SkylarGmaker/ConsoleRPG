fe7bfb538f7d8241fc2adf5739b15a51d8f5da6b
Skylar <skylargartamaker22@gmail.com>
2019-08-31 06:54:12 -0500

    Uploaded the game

---
+++ ConsoleRPG/Game.cpp
@@ -0,0 +1,36 @@
+#include "Game.h"
+
+Game::Game()
+{
+	choice = 0;
+	playing = true; 
+}
+
+
+Game::~Game()
+{
+
+}
+
+//Functions
+void Game::mainMenu()
+{
+	cout << "MAIN MENU" << endl; 
+	cout << "0: Quit" << endl; 
+	cout << "1: Travel" << endl;
+	cout << "2: Shop" << endl;
+	cout << "3: Level up" << endl;
+	cout << "4: Rest" << endl;
+
+	cin >> choice;
+
+	switch (choice)
+	{
+		case 0:
+			playing = false;
+			break;
+		default:
+			break;
+	}
+
+}
